4d9e6d9dd1e2ccde129822d83e660af463718dba
Diego <diegodsha@hotmail.com>
2021-06-22 14:52:56 -0500

    Import all assets to load on build

--- src/test/helpers.test.js
+++ src/test/helpers.test.js
@@ -1,13 +1,13 @@
-import * as helpers from '../helpers'
+import * as helpers from '../helpers';
 
-describe('Check if the input value is a word or a set of numbers',() => {
-    const field = {value: ''}
-    it('returns false if value is empty', () => {
-        expect(helpers.inputValidator(field)).toBeFalsy()
-    })
+describe('Check if the input value is a word or a set of numbers', () => {
+  const field = { value: '' };
+  it('returns false if value is empty', () => {
+    expect(helpers.inputValidator(field)).toBeFalsy();
+  });
 
-     it('returns true if value is a valid word or numer', () => {
-         field.value = 'shagui23'
-        expect(helpers.inputValidator(field)).toBeTruthy()
-    })
-})
+  it('returns true if value is a valid word or numer', () => {
+    field.value = 'shagui23';
+    expect(helpers.inputValidator(field)).toBeTruthy();
+  });
+});
